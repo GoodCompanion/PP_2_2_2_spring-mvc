54e693185cab9c2c55eed8c02a899b16acf4e302
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-03-24 19:54:06 +0400

    Исправил валидацию в контроллере и убрал ненужные импорты в других классах

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,13 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -23,10 +19,10 @@ public class CarController {
     public String showCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
         List<Car> cars;
 
-        if (count != null && count > 0) {
-            cars = carService.getCars(count);  // ← сервис сам решит, сколько вернуть
+        if (count != null) {
+            cars = carService.getCars(count);
         } else {
-            cars = carService.getAllCars();    // ← все машины
+            cars = carService.getAllCars();
         }
         model.addAttribute("cars", cars);
         return "cars";
