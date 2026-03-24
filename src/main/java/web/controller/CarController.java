27f18b24871805d56e159709076a656584a1f1d9
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-03-24 18:20:24 +0400

    Добавил html файл cars и изменил реализацию контроллера

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,6 +1,7 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import web.model.Car;
 
@@ -11,14 +12,14 @@ import java.util.List;
 public class CarController {
 
     @GetMapping(value = "/cars")
-    public String getCars() {
+    public String showCars(Model model) {
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("model1", 50, 6));
         cars.add(new Car("model2", 75, 5));
         cars.add(new Car("model3", 100, 4));
         cars.add(new Car("model4", 125, 3));
         cars.add(new Car("model5", 150, 2));
-
-        return "index";
+        model.addAttribute("cars", cars);
+        return "cars";
     }
 }
